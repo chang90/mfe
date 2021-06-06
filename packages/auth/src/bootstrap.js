2e56e7dd9b40c61eb93e9e385fd975654c45568c
chang90 <chang.liu.programmer@gmail.com>
2021-06-06 16:42:50 +1000

    add auth pages

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,11 +4,9 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './app';
 
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-  const history = 
-    defaultHistory || 
-    createMemoryHistory({
-      initialEntries: [initialPath]
-    });
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath]
+  });
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -30,7 +28,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 };
 
 if(process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
